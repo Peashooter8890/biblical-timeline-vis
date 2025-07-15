dd410a8cf38472b4f34d7b3fc8d5e126ebdd8652
Peashooter8890 <peashooterking8@gmail.com>
2025-07-14 20:28:55 -0400

    ready for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   root: '.',
   build: {
     outDir: 'dist'
@@ -16,4 +17,4 @@ export default defineConfig({
       '@': '/src'
     }
   }
-})
+});
